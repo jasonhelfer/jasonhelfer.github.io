236afd1f49fa4ace2c0a7536acf75fff9d78d177
jasonhelfer <111073124+jasonhelfer@users.noreply.github.com>
2025-03-17 14:30:44 +0000

    pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,13 +10,40 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  const BOARD_WIDTH = $("#board").width();
+  const BOARD_HEIGHT = $("#board").height();
+  const PADDLE_HEIGHT = $("#paddleLeft").height();
+  const BALL_HEIGHT = $("#ball").height();
+  const BALL_WIDTH = $("#ball").width();
   // Game Item Objects
+  const KEY = {
+    "W": 87,
+    "S": 83,
+    "UP": 38,
+    "DOWN": 40,
+  }
 
 
+  function GameItem(id, speedX, speedY){
+    var obj = {
+      id: id,
+      x: parseFloat($(id).css("left")),
+      y: parseFloat($(id).css("top")),
+      speedX: speedX,
+      speedY: speedY,
+      w: $(id).width(),
+      h: $(id).height(),
+    }
+    return obj
+  }
+
+  var paddleLeft = GameItem("#paddleLeft", 0, 0);
+  var paddleRight = GameItem("#paddleRight", 0, 0);
+  var ball = GameItem("#ball",(Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1), (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1) )
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);
+  $(document).on('keyup', handleKeyUp);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,22 +54,97 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    drawGameItem(paddleLeft);
+    updateGameItem(paddleLeft);
+    drawGameItem(paddleRight);
+    updateGameItem(paddleRight);
+    paddleWallCollision(paddleLeft);
+    paddleWallCollision(paddleRight);
+    drawGameItem(ball);
+    updateGameItem(ball);
+    ballWallCollision(ball);
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    if(event.which === KEY.W){
+      paddleLeft.speedY = -5;
+    }
+    if(event.which === KEY.S){
+      paddleLeft.speedY = 5;
+    }
+    if(event.which === KEY.UP){
+      paddleRight.speedY = -5;
+    }
+    if(event.which === KEY.DOWN){
+      paddleRight.speedY = 5;
+    }
+  }
 
+  function handleKeyUp(event) {
+    if(event.which === KEY.W){
+      paddleLeft.speedY = 0;
+    }
+    if(event.which === KEY.S){
+      paddleLeft.speedY = 0;
+    }
+    if(event.which === KEY.UP){
+      paddleRight.speedY = 0;
+    }
+    if(event.which === KEY.DOWN){
+      paddleRight.speedY = 0;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  // Movement Helper
+  function drawGameItem(obj){
+    $(obj.id).css("left", obj.x);
+    $(obj.id).css("top", obj.y);
+  }
 
+  function updateGameItem(obj){
+    obj.x += obj.speedX;
+    obj.y += obj.speedY;
+  }
   
+  function paddleWallCollision(obj){
+    if(obj.y > BOARD_HEIGHT - PADDLE_HEIGHT){
+      obj.speedY = 0;
+      obj.y = BOARD_HEIGHT - PADDLE_HEIGHT;
+  }
+  if(obj.y < 0){
+    obj.speedY = 0;
+    obj.y = 0;
+  }
+}
+function ballWallCollision(obj){
+  if(obj.y > BOARD_HEIGHT - BALL_HEIGHT){
+    obj.speedY = -obj.speedY;
+}
+if(obj.y < 0){
+  obj.speedY = -obj.speedY;
+}
+if(obj.x > BOARD_WIDTH - BALL_WIDTH){
+  obj.speedX = -obj.speedX;
+}
+if(obj.x < 0){
+  obj.speedX = -obj.speedX;
+}
+}
+
+//check boundaries of paddles
+//handle what happens when ball hits the walls
+//handle what happpens when the ball hits the paddles
+//handle what happens when someone wins
+//handles the points
+//handles resetting the game
+
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
