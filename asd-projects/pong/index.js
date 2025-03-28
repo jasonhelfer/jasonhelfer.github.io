a6eccaccdb365aa87095e30877ad856b92ae3a72
jasonhelfer <111073124+jasonhelfer@users.noreply.github.com>
2025-03-28 15:09:37 +0000

    pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -1,7 +1,7 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
+
 function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
@@ -13,6 +13,7 @@ function runProgram(){
   const BOARD_WIDTH = $("#board").width();
   const BOARD_HEIGHT = $("#board").height();
   const PADDLE_HEIGHT = $("#paddleLeft").height();
+  const PADDLE_WIDTH = $("#paddleLeft").width();
   const BALL_HEIGHT = $("#ball").height();
   const BALL_WIDTH = $("#ball").width();
   // Game Item Objects
@@ -40,6 +41,11 @@ function runProgram(){
   var paddleLeft = GameItem("#paddleLeft", 0, 0);
   var paddleRight = GameItem("#paddleRight", 0, 0);
   var ball = GameItem("#ball",(Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1), (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1) )
+  var scoreLeft = GameItem("#scoreLeft", 0, 0);
+  var scoreRight = GameItem("#scoreRight", 0, 0);
+  var scoreLeftNumber = 0;
+  var scoreRightNumber = 0;
+  var offset = 1;
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);
@@ -63,6 +69,13 @@ function runProgram(){
     drawGameItem(ball);
     updateGameItem(ball);
     ballWallCollision(ball);
+    doCollide(ball, paddleLeft);
+    doCollide(ball, paddleRight);
+    $("#scoreLeft").text(scoreLeftNumber);
+    $("#scoreRight").text(scoreRightNumber);
+    scoring(paddleLeft, scoreLeft);
+    scoring(paddleRight, scoreRight);
+    playAgain();
   }
   
   /* 
@@ -71,30 +84,38 @@ function runProgram(){
   function handleKeyDown(event) {
     if(event.which === KEY.W){
       paddleLeft.speedY = -5;
+      scoreLeft.speedY = -10;
     }
     if(event.which === KEY.S){
       paddleLeft.speedY = 5;
+      scoreLeft.speedY = -10;
     }
     if(event.which === KEY.UP){
       paddleRight.speedY = -5;
+      scoreRight.speedY = -10;
     }
     if(event.which === KEY.DOWN){
       paddleRight.speedY = 5;
+      scoreRight.speedY = -10;
     }
   }
 
   function handleKeyUp(event) {
     if(event.which === KEY.W){
       paddleLeft.speedY = 0;
+      scoreLeft.speedY = -10;
     }
     if(event.which === KEY.S){
       paddleLeft.speedY = 0;
+      scoreLeft.speedY = -10;
     }
     if(event.which === KEY.UP){
       paddleRight.speedY = 0;
+      scoreRight.speedY = -10;
     }
     if(event.which === KEY.DOWN){
       paddleRight.speedY = 0;
+      scoreRight.speedY = -10;
     }
   }
 
@@ -106,7 +127,7 @@ function runProgram(){
     $(obj.id).css("left", obj.x);
     $(obj.id).css("top", obj.y);
   }
-
+//brogans code
   function updateGameItem(obj){
     obj.x += obj.speedX;
     obj.y += obj.speedY;
@@ -135,7 +156,46 @@ if(obj.x > BOARD_WIDTH - BALL_WIDTH){
 if(obj.x < 0){
   obj.speedX = -obj.speedX;
 }
+if (obj.x > BOARD_WIDTH - BALL_WIDTH){
+  obj.speedX = -obj.speedX;
+  resetBall(ball);
+  scoreLeftNumber = scoreLeftNumber + 1;
 }
+if (obj.x < 0){
+  obj.speedX = -obj.speedX;
+  resetBall(ball);
+  scoreRightNumber = scoreRightNumber + 1;
+}
+}
+
+function doCollide (ball, paddle){
+  if (ball.x < paddle.x + PADDLE_WIDTH && ball.x > paddle.x - PADDLE_WIDTH && ball.y < paddle.y + PADDLE_HEIGHT && ball.y > paddle.y){
+    ball.speedX = -ball.speedX * 1.2;
+  }
+}
+function scoring (paddle, score){
+  score.y = paddle.y + offset;
+}
+function checkWin(){
+  if (scoreLeft >= WINNING_SCORE){
+    endGame("RED Wins!");
+  } else if (scoreRight >= WINNING_SCORE){
+    endGame("BLUE Wins!");
+  }
+}
+function resetBall (ball){
+  ball.x = (BOARD_WIDTH - BALL_WIDTH) / 2;
+  ball.y = (BOARD_HEIGHT - BALL_HEIGHT) / 2;
+  ball.speedX = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -3 : 1);
+  ball.speedY = (Math.random() > 0.5 ? -3 : 1);
+}
+
+function playAgain(){
+  $("#reset").on("click", reset);
+}
+
+
+
 
 //check boundaries of paddles
 //handle what happens when ball hits the walls
